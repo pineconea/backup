3cee335a8668d90954dfbabf69db4ac3e034739b
wsk <648796308@qq.com>
2014-12-25 13:06:06 +0800

    xx

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+go get -u github.com/nsf/gocode
+go get -u github.com/jstemmer/gotags
 mkdir -p $HOME/.vim/ftdetect
 mkdir -p $HOME/.vim/syntax
 mkdir -p $HOME/.vim/autoload/go
